d2fa2ab8b4aec7050db202c904991f1b1b3bf91f
MBugajski <michalbugajski@outlook.com>
2018-02-10 11:00:04 +0100

    Tutorial 37 try with resources

---
+++ Tutorial 37 try with resources/src/App2.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class App2 {
+	public static void main(String[] args) {
+		File file = new File("text.txt");
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(file))){
+			
+			String line;
+			while( (line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Can't find file: " + file.toString());
+		} catch (IOException e) {
+			System.out.println("Can't read file: " + file.toString());
+		}
+	}
+}
